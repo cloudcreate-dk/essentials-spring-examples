19bb530b3991d04be78c8906a9d01bc123f2d60c
Jeppe Cramon <96371136+cloudcreate-dk@users.noreply.github.com>
2023-03-27 21:20:14 +0200

    Added example `DurableQueuesInterceptor`

---
+++ mongodb-inbox-outbox/src/main/java/dk/cloudcreate/essentials/spring/examples/mongodb/messaging/config/ExampleDurableQueuesInterceptor.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dk.cloudcreate.essentials.spring.examples.mongodb.messaging.config;
+
+import dk.cloudcreate.essentials.components.foundation.messaging.queue.*;
+import dk.cloudcreate.essentials.components.foundation.messaging.queue.operations.*;
+import dk.cloudcreate.essentials.shared.interceptor.InterceptorChain;
+import org.slf4j.*;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+public class ExampleDurableQueuesInterceptor implements DurableQueuesInterceptor {
+    private static final Logger log = LoggerFactory.getLogger(ExampleDurableQueuesInterceptor.class);
+
+    @Override
+    public QueueEntryId intercept(QueueMessage operation, InterceptorChain<QueueMessage, QueueEntryId, DurableQueuesInterceptor> interceptorChain) {
+        log.info("Intercepting: {}", operation);
+        return interceptorChain.proceed();
+    }
+
+    @Override
+    public List<QueueEntryId> intercept(QueueMessages operation, InterceptorChain<QueueMessages, List<QueueEntryId>, DurableQueuesInterceptor> interceptorChain) {
+        log.info("Intercepting: {}", operation);
+        return interceptorChain.proceed();
+    }
+
+    @Override
+    public Optional<QueuedMessage> intercept(GetNextMessageReadyForDelivery operation, InterceptorChain<GetNextMessageReadyForDelivery, Optional<QueuedMessage>, DurableQueuesInterceptor> interceptorChain) {
+        log.info("Intercepting: {}", operation);
+        var nextMessageReadyForDelivery = interceptorChain.proceed();
+        nextMessageReadyForDelivery.ifPresent(queuedMessage -> {
+            log.info("Intercepting GetNextMessageReadyForDelivery: {}", queuedMessage);
+        });
+        return nextMessageReadyForDelivery;
+    }
+}

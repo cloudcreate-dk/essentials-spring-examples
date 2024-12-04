32fb50324179b2ac07acb9551a184c6aee70e705
Jeppe Cramon <96371136+cloudcreate-dk@users.noreply.github.com>
2024-12-04 09:31:20 +0100

    Added `Task` / `TaskProcessor` example that uses the new `InTransactionEventProcessor` and which tests modifying the same aggregate multiple times in the same UnitOfWork through a combination of Commands and Event handlers

---
+++ postgresql-cqrs/src/main/java/dk/cloudcreate/essentials/spring/examples/postgresql/cqrs/task/TaskEventStoreRepository.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2021-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dk.cloudcreate.essentials.spring.examples.postgresql.cqrs.task;
+
+import dk.cloudcreate.essentials.components.eventsourced.aggregates.stateful.StatefulAggregateRepository;
+import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.ConfigurableEventStore;
+import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.eventstream.AggregateType;
+import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.persistence.table_per_aggregate_type.SeparateTablePerAggregateEventStreamConfiguration;
+import dk.cloudcreate.essentials.spring.examples.postgresql.cqrs.task.commands.CreateTask;
+import dk.cloudcreate.essentials.spring.examples.postgresql.cqrs.task.domain.Task;
+import dk.cloudcreate.essentials.spring.examples.postgresql.cqrs.task.domain.TaskId;
+import dk.cloudcreate.essentials.spring.examples.postgresql.cqrs.task.domain.events.TaskEvent;
+import lombok.NonNull;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+import static dk.cloudcreate.essentials.components.eventsourced.aggregates.stateful.StatefulAggregateInstanceFactory.reflectionBasedAggregateRootFactory;
+
+@Component
+public class TaskEventStoreRepository {
+
+    public static final AggregateType AGGREGATE_TYPE = AggregateType.of("Task");
+    private final ConfigurableEventStore<SeparateTablePerAggregateEventStreamConfiguration> eventStore;
+    private final StatefulAggregateRepository<TaskId, TaskEvent, Task> repository;
+
+    public TaskEventStoreRepository(@NonNull ConfigurableEventStore<SeparateTablePerAggregateEventStreamConfiguration> eventStore) {
+        this.eventStore = eventStore;
+        repository = StatefulAggregateRepository.from(eventStore,
+                AGGREGATE_TYPE,
+                reflectionBasedAggregateRootFactory(),
+                Task.class);
+    }
+
+    public Optional<Task> findTask(@NonNull TaskId taskId) {
+        return repository.tryLoad(taskId);
+    }
+
+    public Task getTask(@NonNull TaskId taskId) {
+        return repository.load(taskId);
+    }
+
+    public Task createTask(@NonNull TaskId taskId, CreateTask cmd) {
+        var task = new Task(taskId, cmd);
+        return repository.save(task);
+    }
+}
---
+++ postgresql-cqrs/src/main/java/dk/cloudcreate/essentials/spring/examples/postgresql/cqrs/task/TaskProcessor.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2021-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dk.cloudcreate.essentials.spring.examples.postgresql.cqrs.task;
+
+import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.eventstream.AggregateType;
+import dk.cloudcreate.essentials.components.eventsourced.eventstore.postgresql.processor.*;
+import dk.cloudcreate.essentials.components.foundation.messaging.MessageHandler;
+import dk.cloudcreate.essentials.reactive.command.CmdHandler;
+import dk.cloudcreate.essentials.spring.examples.postgresql.cqrs.task.commands.*;
+import dk.cloudcreate.essentials.spring.examples.postgresql.cqrs.task.domain.Task;
+import dk.cloudcreate.essentials.spring.examples.postgresql.cqrs.task.domain.events.TaskCreated;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.Objects.nonNull;
+
+@Slf4j
+@Service
+public class TaskProcessor extends InTransactionEventProcessor {
+
+    private final TaskEventStoreRepository taskEventStoreRepository;
+
+    protected TaskProcessor(TaskEventStoreRepository taskEventStoreRepository,
+                            EventProcessorDependencies eventProcessorDependencies) {
+        super(eventProcessorDependencies, true);
+        this.taskEventStoreRepository = taskEventStoreRepository;
+    }
+
+    public TaskEventStoreRepository getTaskEventStoreRepository() {
+        return taskEventStoreRepository;
+    }
+
+    @Override
+    public String getProcessorName() {
+        return "TaskProcessor";
+    }
+
+    @Override
+    protected List<AggregateType> reactsToEventsRelatedToAggregateTypes() {
+        return List.of(TaskEventStoreRepository.AGGREGATE_TYPE);
+    }
+
+    @CmdHandler
+    public void handle(CreateTask cmd) {
+        log.info("Creating task with command '{}'", cmd);
+        taskEventStoreRepository.createTask(cmd.taskId(), cmd);
+
+    }
+
+    @CmdHandler
+    public void handle(AddComment cmd) {
+        log.info("Adding comment '{}'", cmd);
+        Task task = taskEventStoreRepository.findTask(cmd.taskId()).orElseThrow();
+        task.addComment(cmd);
+    }
+
+    @MessageHandler
+    void handle(TaskCreated event) {
+        if (nonNull(event.getComment())) {
+            log.info("Task '{}' contains comment adding comment command", event);
+            commandBus.send(new AddComment(event.getTaskId(), event.getComment(), event.getCreatedAt()));
+        }
+    }
+}
